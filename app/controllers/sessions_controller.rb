d1dd427c00c972d6b3b5005ec4f7aa8091a444ca
gsridhar <sridhar.ganta@chronus.com>
2013-07-05 15:54:32 +0530

    end of sign-in-out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-    	render 'new'
-  	end
+      user = User.find_by_email(params[:session][:email].downcase)
+	  if user && user.authenticate(params[:session][:password])
+	    # Sign the user in and redirect to the user's show page.
+	    sign_in user
+        redirect_to user
+	  else
+	  	flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+	    # Create an error message and re-render the signin form.
+	    render 'new'
+	  end
+	end
 
 	def destroy
-	end
+    	sign_out
+    	puts "check"
+    	redirect_to signin_path
+  	end
 end
